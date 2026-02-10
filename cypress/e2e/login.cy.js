76035cf71a17eebf77d2a44dbc33e3d5b9005368
Rizal Abimanyu <rizalabimanyu1102@gmail.com>
2026-02-10 13:42:08 +0700

    make test error 2

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,7 +2,7 @@ describe('Login Flow', () => {
   it('should login successfully', () => {
     cy.visit('http://localhost:5173/login');
 
-    cy.get('input[placeholder="EMAIL"]').type('qazqaz@qazqaz.com');
+    cy.get('input[placeholder="EMAIL"]').type('qazqaz@d.com');
     cy.get('input[placeholder="KATA SANDI"]').type('qazqazqaz');
 
     cy.contains('Login').click();
